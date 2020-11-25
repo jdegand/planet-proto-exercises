6e42a206a64224f4544b4fc9901a21cf037c0b19
jdegand <70610011+jdegand@users.noreply.github.com>
2020-11-25 14:24:30 -0500

    initial commit

---
+++ 010.js
@@ -0,0 +1,18 @@
+// -> Create an object called 'robot' using an object literal
+// -> robot should have a property 'smart' with value true
+__
+
+let robot = {
+	smart: true
+}
+
+
+
+// -> Claim the result robot.smart
+claim(robot.smart, robot.smart);
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+	robot: robot
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# planet-proto-exercises
+Node School Course Planet Proto exercise solutions
+
+[Node School](https://nodeschool.io/#workshoppers)
